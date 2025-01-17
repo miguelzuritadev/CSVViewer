b9ac39bc08f1bb61ebeb12f524342e1fb7392e04
mike <mike@developer.com>
2025-01-17 00:13:52 -0300

    + window maximized

--- composeApp/src/desktopMain/kotlin/com/tools/csv_viewer/main.kt
+++ composeApp/src/desktopMain/kotlin/com/tools/csv_viewer/main.kt
@@ -1,10 +1,9 @@
 package com.tools.csv_viewer
 
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import java.awt.Toolkit
@@ -17,8 +16,9 @@ fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "CSVViewer",
-        state = rememberWindowState(size = windowSize, position = WindowPosition.Aligned(Alignment.Center), isMinimized = false)
-    ) {
+//        state = rememberWindowState(size = windowSize, position = WindowPosition.Aligned(Alignment.Center), isMinimized = false)
+        state = rememberWindowState(placement = WindowPlacement.Maximized),
+        ) {
         App()
     }
 }
